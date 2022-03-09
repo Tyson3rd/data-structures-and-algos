a71d8f6fcba1b37a24f883d1411f1ccfd8d158ba
Tyson3rd <81340496+Tyson3rd@users.noreply.github.com>
2022-03-09 15:40:07 -0600

    pair-sum

--- algos/pair-sum/index.js
+++ algos/pair-sum/index.js
@@ -1,5 +1,17 @@
 function pairSum(arr, sum){
 	// Complete the function!
+	// Arr = array, sum is expected result.
+	//return true or false if any combination += sum
+	//with var for length lowest was: 0.263
+	//with arr.length lewest was: 0.266
+	let len = arr.length;
+	for(let i= 0; i < len; i++){
+		for (let n = i; n < len; n++){
+			//console.log(`Sum: ${arr[i]}+${arr[n]}:`, arr[i] + arr[n]);
+			if(sum == (arr[i] + arr[n]) && arr[i] != arr[n]){ return true; }
+		}
+	}
+	return false;
 }
 
 module.exports = pairSum
