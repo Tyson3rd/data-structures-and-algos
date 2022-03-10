9976298b04b9cc40ecf6879acd8fbb447dd0329b
Tyson3rd <81340496+Tyson3rd@users.noreply.github.com>
2022-03-10 15:06:00 -0600

    multiArrSum

--- algos/recursion/countdown/index.js
+++ algos/recursion/countdown/index.js
@@ -1,5 +1,28 @@
-function countdown(n){
+// // program to count down numbers to 1
+// function countdown(number) {
 
+//     // display the number
+//     console.log(number);
+
+//     // decrease the number value
+//     const newNumber = number - 1;
+
+//     // base case
+//     if (newNumber > 0) {
+//         countdown(newNumber);
+//     }
+// }
+
+// countdown(4);
+
+function countdown(n) {
+    let countdownString = '';
+    if (n <= 0) {
+        return countdownString + 'BlastOff!';
+    } else {
+        countdownString += `${n}, `;
+    }
+    return countdownString += countdown(n - 1)
 }
-
+//  countdown(4)
 module.exports = countdown
--- algos/recursion/countdown/index.test.js
+++ algos/recursion/countdown/index.test.js
@@ -3,19 +3,19 @@ let countdown = require('./index');
 describe("countdown algorithm", () => {
 
 
-	it("returns a string, counting down form a number n to 'Liftoff!' (0)", () => {
+	it("returns a string, counting down form a number n to 'BlastOff!' (0)", () => {
 
-		expect(countdown(9)).toBe("9, 8, 7, 6, 5, 4, 3, 2, 1, Liftoff!");
-		expect(countdown(10)).toBe("10, 9, 8, 7, 6, 5, 4, 3, 2, 1, Liftoff!");
-		expect(countdown(1)).toBe("1, Liftoff!");
+		expect(countdown(9)).toBe("9, 8, 7, 6, 5, 4, 3, 2, 1, BlastOff!");
+		expect(countdown(10)).toBe("10, 9, 8, 7, 6, 5, 4, 3, 2, 1, BlastOff!");
+		expect(countdown(1)).toBe("1, BlastOff!");
 
 	});
 
-	it('returns "Liftoff!" when input is 0', () => {
-		expect(countdown(0)).toBe("Liftoff!");
+	it('returns "BlastOff!" when input is 0', () => {
+		expect(countdown(0)).toBe("BlastOff!");
 	});
 
-	it('returns "Liftoff!" when input is negative', () => {
-		expect(countdown(-100)).toBe("Liftoff!");
+	it('returns "BlastOff!" when input is negative', () => {
+		expect(countdown(-100)).toBe("BlastOff!");
 	});
 })
